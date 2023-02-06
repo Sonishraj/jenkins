5e3c6b2ed19889e7ce2a8eaea509e1b0d94b6865
Sonish Raj <49720238+Sonishraj@users.noreply.github.com>
2023-02-06 12:18:41 -0500

    Update HelloWorldController.java
    changing return statement

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World! Team. Happpy weekend and Happy devops!...Happy developemnt";
+        return "Hello, World! Team. Happpy weekend and Happy devops!...Happy developemnt version 14";
     }
 }
